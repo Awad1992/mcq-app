d648af6cc39232c9bd272fcca26f882686e91cb2
Awad1992 <134640493+Awad1992@users.noreply.github.com>
2025-11-18 18:18:31 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('mcq-ultra-v41').then((cache) => {
+    caches.open('mcq-ultra-v43').then((cache) => {
       return cache.addAll([
         './',
         './index.html',
-        './style.css?v=4.1.0',
-        './app.js?v=4.1.0',
-        './manifest.json?v=4.1.0'
+        './style.css?v=4.3.0',
+        './app.js?v=4.3.0',
+        './manifest.json?v=4.3.0'
       ]);
     })
   );
