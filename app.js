d67990c20115b5e7f5eaeadc1ffd154e8a4d9594
Awad1992 <134640493+Awad1992@users.noreply.github.com>
2025-11-19 13:02:06 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2920,7 +2920,7 @@ openDB().then(() => {
 }).catch(err => {
   console.error(err);
   alert('Failed to open local database.');
-});
+});});
 
 // All tab subview toggle (bank vs builder)
 (function () {
@@ -2944,7 +2944,7 @@ openDB().then(() => {
 })();
 
 // Builder helpers: prompt + preview + import
-let builderPreviewCache = [];
+builderPreviewCache = [];
 
 function makeBuilderPrompt() {
   const src = (document.getElementById('builderSourceText')?.value || '').trim();
